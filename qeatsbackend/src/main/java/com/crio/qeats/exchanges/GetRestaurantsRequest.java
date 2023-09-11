f9a3609cbd552c2d731dc13a280c2c4cb3554043
Ankit92110 <ankitmishra28799@gmail.com>
2023-09-11 12:45:03 +0530

    first commit

---
+++ qeatsbackend/src/main/java/com/crio/qeats/exchanges/GetRestaurantsRequest.java
@@ -0,0 +1,65 @@
+/*
+ *
+ *  * Copyright (c) Crio.Do 2019. All rights reserved
+ *
+ */
+
+package com.crio.qeats.exchanges;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+// TODO: CRIO_TASK_MODULE_RESTAURANTSAPI
+//  Implement GetRestaurantsRequest.
+//  Complete the class such that it is able to deserialize the incoming query params from
+//  REST API clients.
+//  For instance, if a REST client calls API
+//  /qeats/v1/restaurants?latitude=28.4900591&longitude=77.536386&searchFor=tamil,
+//  this class should be able to deserialize lat/long and optional searchFor from that.
+// @Data
+// @NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown=true)
+public class GetRestaurantsRequest {
+   @NotNull
+   @Max(90)
+   @Min(-90)
+   private Double latitude;
+   @NotNull
+   @Max(180)
+   @Min(-180)
+   private Double longitude;
+   private String searchFor;
+   public Double getLatitude() {
+      return latitude;
+   }
+   public GetRestaurantsRequest(Double latitude, Double longitude) {
+      this.latitude = latitude;
+      this.longitude = longitude;
+   }
+   public void setLatitude(Double latitude) {
+      this.latitude = latitude;
+   }
+   public Double getLongitude() {
+      return longitude;
+   }
+   public void setLongitude(Double longitude) {
+      this.longitude = longitude;
+   }
+   public String getSearchFor() {
+      return searchFor;
+   }
+   public void setSearchFor(String searchFor) {
+      this.searchFor = searchFor;
+   }
+   
+   
+   
+
+
+}
+
